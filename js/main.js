8c4d5dd33a1ba6c07e7e683a786b8038204e78f3
Udell <carolineudell@gmail.com>
2022-11-11 16:12:44 -0500

    rm flexslider.css rm flexslider.min.js

--- js/main.js
+++ js/main.js
@@ -122,29 +122,6 @@
 
 	};
 
-	var sliderMain = function() {
-
-	  	$('#hero .flexslider').flexslider({
-			animation: "fade",
-			slideshowSpeed: 5000,
-			directionNav: true,
-			start: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			},
-			before: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			}
-
-	  	});
-
-	};
-
 	var stickyFunction = function() {
 
 		var h = $('.image-content').outerHeight();
@@ -189,7 +166,6 @@
 		contentWayPoint();
 		burgerMenu();
 		mobileMenuOutsideClick();
-		sliderMain();
 		stickyFunction();
 	});
 
